1a18038470021ad7d40b5888c7bf0d773067a471
Rasmus Bremholm <rasmus.brem@gmail.com>
2023-08-22 13:03:04 +0200

    pokemon font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import "@/app/styles/globals.scss";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 
-const inter = Inter({ subsets: ["latin"] });
+const pokeFont = localFont({ src: "pokemon-font.ttf" });
 
 export const metadata = {
 	title: "Create Next App",
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body className={pokeFont.className}>{children}</body>
 		</html>
 	);
 }
