ecfadd7b7d840bd9bd698f4b1a0f22075fd1aacc
Rasmus Bremholm <rasmus.brem@gmail.com>
2023-08-23 09:07:16 +0200

    variables finish, navbar start

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -1,5 +1,8 @@
-import React from "react";
-
-export const navbar = () => {
-	return <div>navbar</div>;
+export const Navbar = () => {
+	return (
+		<nav>
+			<div></div>
+			<div></div>
+		</nav>
+	);
 };
