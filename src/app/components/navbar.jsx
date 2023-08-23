f286ac3708019d64a24d7f578d29a6e96171a28d
Rasmus Bremholm <rasmus.brem@gmail.com>
2023-08-23 21:15:32 +0200

    navbar continued work

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -1,13 +1,20 @@
 import styles from "@/app/styles/Navbar.module.scss";
+import Image from "next/image";
 
 export const Navbar = () => {
 	return (
 		<nav className={styles.Navbar}>
-			<div>
+			<div className={styles.Logo}>
+				<Image
+					src="/pokemon-icon.png"
+					alt="pokemon icon"
+					width={67}
+					height={68}
+				/>
 				<h1>Pokemon</h1>
 			</div>
-			<div>
-				<p>Searc2</p>
+			<div className={styles.searchbarContainer}>
+				<p>Search</p>
 			</div>
 		</nav>
 	);
