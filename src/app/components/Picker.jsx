6af401b571477edb3967e05429eb9d6b5592ebf0
Rasmus Bremholm <rasmus.brem@gmail.com>
2023-12-20 21:07:55 +0100

    Cant get Apollo to work

--- src/app/components/Picker.jsx
+++ src/app/components/Picker.jsx
@@ -1,29 +1,7 @@
-"use client";
-
 import styles from "../styles/components/picker.module.scss";
 import Card from "./Card";
-import { getClient } from "@/lib/client";
-import { gql, useQuery } from "@apollo/client";
-import { useState, useEffect } from "react";
 
-const query = gql`
-	query genOnePokemon {
-		gen1_species: pokemon_v2_pokemonspecies(
-			where: { pokemon_v2_generation: { name: { _eq: "generation-i" } } }
-			order_by: { id: asc }
-		) {
-			id
-			name
-		}
-	}
-`;
-
-export default async function Picker() {
-	const { data } = await getClient().query({ query });
-	const pokemons = data.gen1_species;
-
-	console.log(pokemons);
-
+export default function Picker() {
 	return (
 		<div className={styles.picker}>
 			<div className={styles.gridContainer}>
