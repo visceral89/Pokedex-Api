ef5f424e093dfc4af6d13bf87071d16431b22aa1
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-01-17 15:45:56 +0100

    Added more types

--- src/app/components/Type.jsx
+++ src/app/components/Type.jsx
@@ -3,10 +3,24 @@ import styles from "../styles/components/type.module.scss";
 export default function Type({ types }) {
 	const getTypeColor = (typeName) => {
 		const typeColors = {
-			grass: "green",
-			poison: "blue",
-			fire: "red",
-			electric: "yellow",
+			normal: "#a8a878",
+			fire: "#f08030",
+			fighting: "#c03028",
+			water: "#6890f0",
+			flying: "#a890f0",
+			grass: "#78c850",
+			poison: "#a040a0",
+			electric: "#f8d030",
+			ground: "#e0c068",
+			psychic: "#f85888",
+			rock: "#b8a038",
+			ice: "#98d8d8",
+			bug: "#a8b820",
+			dragon: "#7038f8",
+			ghost: "#705898",
+			dark: "#705848",
+			steel: "#b8b8d0",
+			fairy: "#ee99ac",
 		};
 		return typeColors[typeName] || "gray";
 	};
